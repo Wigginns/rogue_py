2c35514164e8813538e329fb3792bbebb4b3e5d4
Wigginns <3180645+Wigginns@users.noreply.github.com>
2019-01-05 21:31:45 -0700

    Part 2 halfway: render_functions and entity class setup and used

--- engine.py
+++ engine.py
@@ -1,13 +1,18 @@
 import tcod as libtcod
 
 from input_handlers import handle_keys
+from entity import Entity
+from render_functions import render_all, clear_all
 
 def main():
     screen_width = 80
     screen_height = 50
+    map_width = 80
+    map_height = 50
 
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
+    player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', libtcod.yellow)
+    entities = [npc, player]
 
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
@@ -21,12 +26,10 @@ def main():
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
-        libtcod.console_set_default_foreground(con, libtcod.white)
-        libtcod.console_put_char(con, player_x, player_y, '@', libtcod.BKGND_NONE)
-        libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
+        render_all(con, entities, screen_width, screen_height)
         libtcod.console_flush()
 
-        libtcod.console_put_char(con, player_x, player_y, ' ', libtcod.BKGND_NONE)
+        clear_all(con, entities)
 
         action = handle_keys(key)
 
@@ -36,8 +39,7 @@ def main():
 
         if move:
             dx, dy = move
-            player_x += dx
-            player_y += dy
+            player.move(dx, dy)
 
         if exit:
             return True
---
+++ render_functions.py
@@ -0,0 +1,23 @@
+import tcod as libtcod
+
+def render_all(con, entities, screen_width, screen_height):
+    #Draw all Entities in the list
+    for entity in entities:
+        draw_entity(con,entity)
+
+    libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
+
+
+def clear_all(con, entities):
+    for entity in entities:
+        clear_entity(con, entity)
+
+
+def draw_entity(con, entity):
+    libtcod.console_set_default_foreground(con, entity.color)
+    libtcod.console_put_char(con, entity.x, entity.y, entity.char, libtcod.BKGND_NONE)
+
+
+def clear_entity(con, entity):
+    #erase the character that represents this Entity
+    libtcod.console_put_char(con, entity.x, entity.y, ' ', libtcod.BKGND_NONE)
